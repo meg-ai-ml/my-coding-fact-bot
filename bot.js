0e9f63c6816eaa0a466738ad8d4c99f64c241ff4
meg-ai-ml <meghanathalakola@gmail.com>
2026-06-21 11:56:59 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "codingBot";
+let botLocation = " the coding universe";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Developer Brendan Eich created the very first version of the language " + favoriteLanguage + "in just 10 days while working at Netscape." 
+console.log(codingFact)
+
+codingFact = "Originally called Mocha: Before settling on" + favoriteLanguage + "the project was briefly named Mocha and then LiveScript."
+console.log(codingFact)
+
+codingFact = "SpaceX utilized Chromium" + favoriteLanguage + " to build the touchscreen flight interfaces for its Crew Dragon spacecraft."
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " +  botLocation + ".")
+
+
+
+
